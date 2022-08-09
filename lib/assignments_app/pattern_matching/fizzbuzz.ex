21829954c7e02dc6d3bb40372f04053f2e002c15
Evangelin95 <avbc95@gmail.com>
2022-08-08 19:41:38 -0500

    inicializacion

--- lib/assignments_app/pattern_matching/fizzbuzz.ex
+++ lib/assignments_app/pattern_matching/fizzbuzz.ex
@@ -1,8 +1,7 @@
 defmodule SophosApp.FizzBuzz do
   # Apply fizz_buzz
   def fizz_buzz(number),
-    do: IO.inspect(Enum.map(1..number, fn n -> fizz_buzz(rem(n, 3), rem(n, 5), n) end))
-
+    do: Enum.map(1..number, fn n -> fizz_buzz(rem(n, 3), rem(n, 5), n) end)
   defp fizz_buzz(0, 0, _n), do: "FizzBuzz"
   defp fizz_buzz(0, _, _n), do: "Fizz"
   defp fizz_buzz(_, 0, _n), do: "Buzz"

07b879203e5cd7d5c50162625fb0ca6771325e28
Sebastian <sebastiancastillogiraldo@gmail.com>
2022-08-15 16:29:16 -0500

    dia 2 estable advent of code

--- lib/assignments_app/advent_of_code/day_3.ex
+++ lib/assignments_app/advent_of_code/day_3.ex
@@ -1,34 +1,51 @@
 defmodule SophosApp.AdventCode.Day3 do
-  @rules fn
-    "^" -> [0, 1]
-    "v" -> [0, -1]
-    ">" -> [1, 0]
-    "<" -> [-1, 0]
-  end
-
   defmodule Star1 do
     def evaluate_star_1(str) do
       str
       |> String.split("", trim: true)
-      |> Enum.map(@rules)
+      |> Enum.map(&rules/1)
       |> Enum.scan(fn [x, y], [x2, y2] -> [x + x2, y + y2] end)
       # initial position of the route
       |> Enum.concat([[0, 0]])
       |> Enum.uniq()
       |> Enum.count()
+    end
 
-      # trying to use zip_with
-      # str
-      # |> String.split("", trim: true)
-      # |> Enum.map(rules)
-      # |> Enum.scan([0, 0], fn x, y ->
-      #   &(Enum.zip_with(&1, &2, fn a, b -> a + b end) |> Enum.to_list())
-      # end)
+    def rules(rule) do
+      %{
+        "^" => [0, 1],
+        "v" => [0, -1],
+        ">" => [1, 0],
+        "<" => [-1, 0]
+      }[rule]
     end
   end
 
-  defmodule Star1 do
+  defmodule Star2 do
     def evaluate_star_2(str) do
+      str
+      |> String.split("", trim: true)
+
+      # [[],[]]
+    end
+
+    defp evaluate_route(route) do
+      # route
+      # |> Enum.map(&rule)
+      # |> Enum.scan(fn [x, y], [x2, y2] -> [x + x2, y + y2] end)
+      # # initial position of the route
+      # |> Enum.concat([[0, 0]])
+      # |> Enum.uniq()
+      # |> Enum.count()
+    end
+
+    def rules(rule) do
+      %{
+        "^" => [0, 1],
+        "v" => [0, -1],
+        ">" => [1, 0],
+        "<" => [-1, 0]
+      }[rule]
     end
   end
 end

536e711156bd40e2259e64194eb8dd46ec26f465
Sebastian <sebastiancastillogiraldo@gmail.com>
2022-09-11 22:58:33 -0500

    tareas de supervisor con factorial

---
+++ lib/assignments_app/supervisor/factorial_supervisor.ex
@@ -0,0 +1,17 @@
+defmodule SophosAppAssignments.FactorialSupervisor do
+  use Supervisor
+  alias SophosAppAssignments.FactorialGenServer
+
+  def start_link(args) do
+    Supervisor.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  def init(_args) do
+    children = [FactorialGenServer]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
+
+
+# {:ok, supervisor} = SophosAppAssignments.FactorialSupervisor.start_link []
